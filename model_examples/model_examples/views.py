b910e3151f42e8259fe519f5ac158ed599c5966a
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-06-11 12:52:39 -0500

    Añadir funcionalidad para renderizar un PDF desde una plantilla HTML usando  y  Actualizar la vista para devolver el PDF generado como un HttpResponse con tipo de contenido .

--- model_examples/model_examples/views.py
+++ model_examples/model_examples/views.py
@@ -12,11 +12,12 @@ class GeneratePDF(View):
         template = get_template('pdf/invoice.html')
         invoice_number = "007cae"
         context = {
-            "bill_to": "Ethan Hunt",
+            "customer_name": "Ethan Hunt",
             "invoice_number": f"{invoice_number}",
             "amount": locale.currency(100_000, grouping=True),
             "date": "2021-07-04",
             "pdf_title": f"Invoice #{invoice_number}",
         }
         html = template.render(context)
-        return HttpResponse(html)
+        pdf = render_to_pdf('pdf/invoice.html', context)
+        return HttpResponse(pdf, content_type='application/pdf')
