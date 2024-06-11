adfeaceac035d32d43dac028d641d48790ba4970
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-06-11 11:47:17 -0500

    Replica de la actividad del video 5 'Database Settings' -- NOTA: esta actividad se hizo al comienzo del proyecto debido a que se opto por el uso de mysql de xampp como la base de datos

--- model_examples/example/models.py
+++ model_examples/example/models.py
@@ -29,7 +29,7 @@ class Framework(models.Model):
     def __str__(self):
         return self.name
         
-# Inicio de replicar actividad del cuarto Video 'Many To Many Quary'.
+#Inicio de replicar actividad del quinto Video 'DataBase Settings'.
     
 class Movie(models.Model):
     name = models.CharField(max_length=10)
@@ -44,5 +44,4 @@ class Character(models.Model):
     def __str__(self):
         return self.name
        
-# Finalizacion de replicar actividad del cuarto Video 'Many To Many Quary'.
         
