86397b1d323add0f5cf2cbcda6506f2fe354c19d
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-06-10 21:33:37 -0500

    Empezando la replica de las actividades de los videos, Definiendo modelos en la aplicación example, En el modelo Framework se hizo uso de la relación ForeignKey a Language

--- model_examples/example/models.py
+++ model_examples/example/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Simple(models.Model):
+    text = models.CharField(max_length=10)
+    number = models.IntegerField(null=True)
+    url = models.URLField(default='www.example.com')
+    
+    def __str__(self):
+        return self.url
+        
+class DateExample(models.Model):
+    the_date = models.DateTimeField()
+    
+class NullExample(models.Model):
+    col = model.CharField(max_length=10, blank=True, null=True)
+    
+class Language(models.Model):
+    name = models.CharField(max_length=10)
+    
+class Framework(models.Model):
+    name = models.CharField(max_length=10)
+    language = models.ForeignKey(language, on_delete=models.CASCADE)
