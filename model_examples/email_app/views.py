e2acdbeb3cb5f420ccd66375d465c5a5802e9587
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-06-12 08:08:17 -0500

    Finalizando replica del septimo video, en vistas se coloco los paramatres de send_mail ('asunto', mensaje, correro, destinatario)

--- model_examples/email_app/views.py
+++ model_examples/email_app/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 # Create your views here.
 
 def index(request):
+    send_mail(
+        'Saludo desde Django',
+        'Hola Mundo!!',
+        'victorgonzalomaldonadovilca@gmail.com',
+        ['vmaldonadov@unsa.edu.pe'],
+    )
     return render(request, "email/index.html")
